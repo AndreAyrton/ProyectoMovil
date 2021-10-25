9dabc6b8ccfd08586178268e66e7032360a124fa
Andrew <2017110745@udh.edu.pe>
2021-10-25 12:25:38 -0500

    Ingreso de datos para al aplicacion

--- app/src/main/java/com/example/proyectomoviles/MainActivity.java
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.proyectomoviles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Registrar=(Button)findViewById(R.id.RegistrarBTN);
+
+        Registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Registro.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/proyectomoviles/PantalladeCarga.java
+++ app/src/main/java/com/example/proyectomoviles/PantalladeCarga.java
@@ -10,5 +10,6 @@ public class PantalladeCarga extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallade_carga);
+
     }
 }
